bfc5600b2567c129f8798a5cb3a58691141db652
tzwm <git@tzwm.me>
2024-10-31 20:21:11 +0800

    update mirror

--- background/main.py
+++ background/main.py
@@ -6,10 +6,14 @@ from dramatiq.middleware import CurrentMessage
 from background import config
 from background.middlewares import Logger
 
-broker = RedisBroker(url=config.broker_redis_url)
+broker = RedisBroker(
+    url=config.broker_redis_url,
+    middleware=[
+        AsyncIO(),
+        CurrentMessage(),
+        Logger(),
+    ],
+)
 dramatiq.set_broker(broker)
-dramatiq.get_broker().add_middleware(AsyncIO())
-dramatiq.get_broker().add_middleware(CurrentMessage())
-dramatiq.get_broker().add_middleware(Logger())
 
 import background.tasks
